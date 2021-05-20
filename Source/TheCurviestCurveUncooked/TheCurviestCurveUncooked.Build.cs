bafe13e815c704b585136b249ab0f6b338dc95cb
Skyler Clark <sclark@freolic.com>
2021-05-20 09:57:40 -0400

    Update for 4.26 by moving K2Node into an UncookedOnly module. - Added CoreRedict to Plugin Config

---
+++ Source/TheCurviestCurveUncooked/TheCurviestCurveUncooked.Build.cs
@@ -0,0 +1,72 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class TheCurviestCurveUncooked : ModuleRules
+{
+	public TheCurviestCurveUncooked(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicIncludePaths.AddRange(
+			new string[] {
+				// ... add public include paths required here ...
+			}
+			);
+				
+		
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				// ... add other private include paths required here ...
+			}
+			);
+			
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"TheCurviestCurve"
+				// ... add other public dependencies that you statically link with here ...
+			}
+			);
+
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore",
+				"InputCore",
+				"AssetTools",
+				"KismetCompiler",
+				"GraphEditor",
+				"BlueprintGraph",
+#if UE_4_24_OR_LATER
+				"ToolMenus",
+#endif
+				// ... add private dependencies that you statically link with here ...	
+			}
+			);
+
+		if (Target.Type == TargetRules.TargetType.Editor)
+		{
+			PrivateDependencyModuleNames.AddRange(
+				new string[]
+				{
+					"UnrealEd",
+					"EditorStyle"
+				});
+		}
+
+
+
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				// ... add any modules that your module loads dynamically here ...
+			}
+			);
+	}
+}
